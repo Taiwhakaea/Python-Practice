2d04281c67010d30715e86dc4731b37bb8488aaa
Taiwhakaea <159851201+Taiwhakaea@users.noreply.github.com>
2024-05-07 12:26:10 +1200

    ortrh

---
+++ main.py
@@ -0,0 +1,8 @@
+#ask the user their name and save it
+name = input ("ni hao")
+#greet the user and introfuce the quiz
+print ("nigga\n", name)
+#ask the user question
+input ("whats your name")
+answer = ("ni haf")
+if else print ("no nigga")
