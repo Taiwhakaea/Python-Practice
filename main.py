e16408b63acb5ff509249400b8c11a95a8398807
Taiwhakaea <159851201+Taiwhakaea@users.noreply.github.com>
2024-05-10 12:44:24 +1200

    fhrh

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-#ask the user their name and save it
+score = 0
+play = "yes"
+
+# Ask the user their name and save it
 name = input ("ni hao")
-#greet the user and introfuce the quiz
-print ("nigga\n", name)
-#ask the user question
-input ("whats your name")
-answer = ("ni haf")
-if else print ("no nigga")
+# Greet the user and introduce the quiz
+print ("ni hao man,", name)
+print ("ching.")
+
+# Ask the user a question
+answer = input("smash or pass\n")
+if answer == "smash".upper():
+    print ("sigma")
+    score +=4
+elif answer == "":
+    print ("stupid")
+else:
+    print ("incorrect stupid")
+print ("ANSWER IS SMASH")
+# End the quiz
+print ("well done ni hao", score, "points")
